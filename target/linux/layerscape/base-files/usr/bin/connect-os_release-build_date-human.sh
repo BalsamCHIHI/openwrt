8076ecd362501ee57d57db3e27b8a420f3c72184
Balsam CHIHI <balsam.chihi@moment.tech>
2025-12-02 18:29:01 +0100

    base-files: usr: bin: Remove useless files

--- target/linux/layerscape/base-files/usr/bin/connect-os_release-build_date-human.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-BUILD_DATE=$(grep OPENWRT_BUILD_DATE /etc/os-release | cut -d '"' -f2)
-date -d "@$BUILD_DATE"
--- target/linux/layerscape/base-files/usr/bin/connect-os_release-build_date-raw.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-BUILD_DATE=$(grep OPENWRT_BUILD_DATE /etc/os-release | cut -d '"' -f2)
-echo "$BUILD_DATE"
